b495052ccbfcf9f84f174e8d89dd8bd3de5c2f4e
CaelDesk <caelsantos71@gmail.com>
2025-11-25 17:40:15 -0300

    adicionado scrollIntoView smooth para as section

--- JS/main.js
+++ JS/main.js
@@ -41,13 +41,44 @@ function initAccordion() {
         this.classList.toggle('ativo');
         this.nextElementSibling.classList.toggle('ativo');
     }
-    
+
     accordionList.forEach((item) => {
         item.addEventListener('click', activeAccordion);
     });
 }
 
 
+function initScrollSmooth() {
+
+    function scrollToSection(event) {
+
+        event.preventDefault();
+        /*capturo a href do link #link*/
+        const href = event.currentTarget.getAttribute('href');
+        /*digo que a seção é o elemento que tenha o ''href''*/
+        const section = document.querySelector(href);
+        section.scrollIntoView({
+
+            behavior: 'smooth',
+            block: 'start'
+        });
+
+    }
+
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+    linksInternos.forEach((link) => {
+
+        link.addEventListener('click', scrollToSection);
+
+    })
+
+
+}
+
+
+
+
+initScrollSmooth();
 initAccordion();
 initTabNav();
 
