282f15f92d0dfb294c4d049fdbcb1fc63ad6dedd
Cael <caelsantos71@gmail.com>
2026-02-03 17:37:04 -0300

    adicionado modelo data-set aos elementos

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,7 @@
 function initTabNav() {
 
-    const tabmenu = document.querySelectorAll('.js-tabmenu li'); //seleciono todos elementos li que estao na classe js-tabmenu
-    const tabcontent = document.querySelectorAll('.js-tabcontent section');//seleciono todas as seções que estao na classe do elemento js-tabcontent
+    const tabmenu = document.querySelectorAll('[data-tab="menu"] li'); //seleciono todos elementos li que estao na classe js-tabmenu
+    const tabcontent = document.querySelectorAll('[data-tab="content"] section');//seleciono todas as seções que estao na classe do elemento js-tabcontent
 
     //faz a verificação se as sections existem e se possuem elementos dentro delas
     if (tabmenu.length && tabcontent.length) {
@@ -13,7 +13,10 @@ function initTabNav() {
                 section.classList.remove('ativo')
             })
             //e aqui eu digo que quando a funcao for chamada, adicionar a seção que contenha o index a classe 'ativo'
-            tabcontent[index].classList.add('ativo');
+            const direction = tabcontent[index].dataset.anime;
+            tabcontent[index].classList.add('ativo' , direction);
+
+            
         }
 
         //outro for each que passa por cada LI e ao ser clicado na eventual LI, envie o index dela para a  funcao de adicionar index à seção
@@ -21,6 +24,7 @@ function initTabNav() {
 
             li.addEventListener('click', () => {
                 activeTab(index);
+                
             })
 
         })
@@ -31,7 +35,7 @@ function initTabNav() {
 
 function initAccordion() {
     //seleciona-se os DT que estao contidos como filho na classe js-accordion
-    const accordionList = document.querySelectorAll(' .js-accordion dt');
+    const accordionList = document.querySelectorAll(' [data-anime="accordion"] dt');
     accordionList[0].classList.add('ativo');
     accordionList[0].nextElementSibling.classList.add('ativo');
 
@@ -82,7 +86,7 @@ function initScrollSmooth() {
 
 
 function animatedScroll(){
-const sections = document.querySelectorAll('.js-scrollSmooth');
+const sections = document.querySelectorAll('[data-anime="scroll"]');
 const windowHeight = window.innerHeight * 0.6;
 
 function scrollSmooth() {
